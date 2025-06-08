82b9cb2bb542cf3cbc99dc5c35c87053b473783a
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-06-08 22:31:03 +0400

    Update Vite configuration base path from "/therapy-code-landing" to "/web" for deployment adjustments.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/therapy-code-landing",
+  base: "/web",
   server: {
     host: '::',
     port: 8080,

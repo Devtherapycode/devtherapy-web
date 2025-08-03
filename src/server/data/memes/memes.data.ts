c235a81160251a1fb54f4364777b0c11089b2a65
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-08-03 16:18:11 +0400

    feat: Remove deprecated meme assets and update meme data
    - Deleted multiple unused meme assets from the project to streamline the asset library.
    - Updated 'memes.data.ts' to reflect the removal of these assets, ensuring the exported memes array is current and accurate.

--- src/server/data/memes/memes.data.ts
+++ src/server/data/memes/memes.data.ts
@@ -2,246 +2,125 @@
  README: Data of memes are sorted by date, `ukan_daixie_js_developero` is the first meme
 */
 
-import zuba_nina_avto from '@/assets/memes/zuba_nina_avto.png';
-import avto_runaway from '@/assets/memes/avto_runaway.mp4';
-import avto_toreto_rav4 from '@/assets/memes/avto_toreto_rav4.png';
-import avto_toreto from '@/assets/memes/avto_toreto.png';
-import intimuri_shuqi from '@/assets/memes/intimuri_shuqi.jpg';
-import dotneteri_neli_kodi from '@/assets/memes/dotneteri_neli_kodi.png';
-import xari_xartan from '@/assets/memes/xari_xartan.png';
-import grdzeli_meme from '@/assets/memes/grdzeli_meme.png';
-import tornike_everyone from '@/assets/memes/tornike_everyone.mov';
-import main_chat from '@/assets/memes/main_chat.jpg';
-import game_jam_sachmeli from '@/assets/memes/game_jam_sachmeli.jpg';
-import zuba_car_story from '@/assets/memes/zuba_car_story.mp4';
-import ai_es_c_sharp from '@/assets/memes/ai_es_c_sharp.jpg';
-import tornikes_multiverse from '@/assets/memes/tornikes_multiverse.png';
-import abela_usulo_tamashi from '@/assets/memes/abela_usulo_tamashi.jpg';
-import zuba_deleting_videos from '@/assets/memes/zuba_deleting_videos.png';
-import three_devs_one_bug_infinite_theories from '@/assets/memes/three_devs_one_bug_infinite_theories.png';
-import abelix_x_zuba from '@/assets/memes/abelix_x_zuba.png';
-import three_babies from '@/assets/memes/three_babies.png';
-import tornike_shaurma from '@/assets/memes/tornike_shaurma.mp4';
-import excel_bug from '@/assets/memes/excel_bug.png';
-import ese_ambavi_martali from '@/assets/memes/ese_ambavi_martali.png';
-import zuba_giyvarvar_tu_gkidivar from '@/assets/memes/zuba_giyvarvar_tu_gkidivar.png'
-import c_vs_java_juniors from '@/assets/memes/c_vs_java_juniors.jpg'
-import zuba_is_typing from '@/assets/memes/zuba_is_typing.jpg';
-import tornike_looks_soso from '@/assets/memes/tornike_looks_soso.png';
-import male from '@/assets/memes/male.jpg'
-import biznes_idea from '@/assets/memes/biznes_idea.mp4';
-import dotnet_vs_gta from '@/assets/memes/dotnet_vs_gta.jpg';
-import mezoblis_bavshvma_gta_chaiwera from '@/assets/memes/mezoblis_bavshvma_gta_chaiwera.jpg';
-import gta_developer_vs_of_model from '@/assets/memes/gta_developer_vs_of_model.jpg';
-import uxilavi_folderiani_dzmakaci from '@/assets/memes/uxilavi_folderiani_dzmakaci.jpg';
-import dotneteri from '@/assets/memes/dotneteri.png';
-import avto_loves_hs from '@/assets/memes/avto_loves_hs.png';
-import tornike_gasaubrebas_atarebs from '@/assets/memes/tornike_gasaubrebas_atarebs.jpg';
-import zuba_roca_gaigo_rom from '@/assets/memes/zuba_roca_gaigo_rom.jpg';
-import zuba_x_dotnet_sastavi from '@/assets/memes/zuba_x_dotnet_sastavi.jpg';
-import c_sharp_is_slow from '@/assets/memes/c_sharp_is_slow.png';
-import website_will_be_dotnet from '@/assets/memes/website_will_be_dotnet.jpg';
-import needtherapy from '@/assets/memes/needtherapy.mp4';
-import avto_draw_25 from '@/assets/memes/avto_draw_25.png';
-import avto_smilling from '@/assets/memes/avto_smilling.png';
-import tornike_silenced from '@/assets/memes/tornike_silenced.png';
-import avto_eating_shawarma from '@/assets/memes/avto_eating_shawarma.mp4';
-import dotnet_neli_tua from '@/assets/memes/dotnet_neli_tua.png';
-import magla_isari_avagde from '@/assets/memes/magla_isari_avagde.png';
-import norqiqa_muzit_xelshi from '@/assets/memes/norqiqa_muzit_xelshi.png';
-import utimod_ro_avawyo from '@/assets/memes/utimod_ro_avawyo.jpg';
-import avto_hate_c_sharp from '@/assets/memes/avto_hate_c_sharp.png';
-import avto_lifts_c_sharp from '@/assets/memes/avto_lifts_c_sharp.jpg';
-import zubas_yoveldgiuroba from '@/assets/memes/zubas_yoveldgiuroba.png';
-import konkretulad_rashia_dotnet_neli from '@/assets/memes/konkretulad_rashia_dotnet_neli.jpg';
-import dotnet_hate_kompetencia from '@/assets/memes/dotnet_hate_kompetencia.jpg';
-import avto_x_dotnet_meetup_members from '@/assets/memes/avto_x_dotnet_meetup_members.jpg';
-import enterprise_devs_vs_other_devs from '@/assets/memes/enterprise_devs_vs_other_devs.jpg';
-import avto_sibereshi from '@/assets/memes/avto_sibereshi.jpg';
-import avto_heartstone_wageba from '@/assets/memes/avto_heartstone_wageba.jpg';
-import avto_hobbies from '@/assets/memes/avto_hobbies.jpg';
-import tornike_asks_render_priest_for_blessing from '@/assets/memes/tornike_asks_render_priest_for_blessing.mp4';
-import avto_c_sharp_demigod_card from '@/assets/memes/avto_c_sharp_demigod_card.png';
-import zuba_x_dotneteri_avto from '@/assets/memes/zuba_x_dotneteri_avto.jpg';
-import avto_c_sharp_pose from '@/assets/memes/avto_c_sharp_pose.jpg';
-import c_sharp_shekreba from '@/assets/memes/c_sharp_shekreba.jpg';
-import va_enterprise_dev from '@/assets/memes/va_enterprise_dev.jpg';
-import tornike_vs_avto from '@/assets/memes/tornike_vs_avto.mp4';
-import avto_enterprise_vs_t_z_r from '@/assets/memes/avto_enterprise_vs_t_z_r.mp4';
-import zubas_gta_leak_uxsene from '@/assets/memes/zubas_gta_leak_uxsene.mp4';
-import dotnet_cudia from '@/assets/memes/dotnet_cudia.png';
-import avto_dancing from '@/assets/memes/avto_dancing.mp4';
-import zuba_x_saba_engine from '@/assets/memes/zuba_x_saba_engine.png';
-import merabi_trump from '@/assets/memes/merabi_trump.jpg';
-import zuba_win_his_game_jam from '@/assets/memes/zuba_win_his_game_jam.mp4';
-import devtherapy_after_midnight from '@/assets/memes/devtherapy_after_midnight.gif';
-import tornike_x_teen_titans from '@/assets/memes/tornike_x_teen_titans.jpg';
-import zuba_no_leak_gta from '@/assets/memes/zuba_no_leak_gta.jpg';
-import tornike_swift_card from '@/assets/memes/tornike_swift_card.png';
-import avto_c_sharp_card from '@/assets/memes/avto_c_sharp_card.png';
-import devtherapy_random_discussions from '@/assets/memes/devtherapy_random_discussions.jpg';
-import devtherapy_dgisit_gamit from '@/assets/memes/devtherapy_dgisit_gamit.jpg';
-import devtherapy_private_party from '@/assets/memes/devtherapy_private_party.gif';
-import devtherapy_paraskevs_cota_xanshi_v2 from '@/assets/memes/devtherapy_paraskevs_cota_xanshi_v2.jpg';
-import devtherapy_paraskevs_cota_xanshi from '@/assets/memes/devtherapy_paraskevs_cota_xanshi.jpg';
-import devtherapy_fear_cota_xanshi from '@/assets/memes/devtherapy_fear_cota_xanshi.jpg';
-import devtherapy_cota_xanshi from '@/assets/memes/devtherapy_cota_xanshi.jpg';
-import tornike_dotnet_dajma from '@/assets/memes/tornike_dotnet_dajma.jpg';
-import t_a_fighting_X_r_sharing_links from '@/assets/memes/t_a_fighting_X_r_sharing_links.mp4';
-import devtherapy_episode_start from '@/assets/memes/devtherapy_episode_start.mp4';
-import gta_sesxi from '@/assets/memes/gta_sesxi.jpg';
-import render_samsaxuri_banki from '@/assets/memes/render_samsaxuri_banki.jpg';
-import trump_x_devtherapy from '@/assets/memes/trump_x_devtherapy.jpg';
-import devtherapy_render_politics from '@/assets/memes/devtherapy_render_politics.jpg';
-import devtherapy_politics from '@/assets/memes/devtherapy_politics.jpg';
-import kurdebi_dependece_injection from '@/assets/memes/kurdebi_dependece_injection.jpg';
-import maikebis_shekveta from '@/assets/memes/maikebis_shekveta.png';
-import how_mau_pg_look from '@/assets/memes/how_mau_pg_look.png';
-import ar_aris_es_tvaleti from '@/assets/memes/ar_aris_es_tvaleti.jpg';
-import tamashis_ideebi from '@/assets/memes/tamashis_ideebi.jpg';
-import ra_waiget_tvini_am_renderit from '@/assets/memes/ra_waiget_tvini_am_renderit.jpg';
-import renderit_samsaxurs_vishovit_xo from '@/assets/memes/renderit_samsaxurs_vishovit_xo.jpg';
-import axali_tamashi_davwerot from '@/assets/memes/axali_tamashi_davwerot.jpg';
-import everyone_reading_render_stuff from '@/assets/memes/everyone_reading_render_stuff.jpg';
-import zuba_spitting_render_stuff from '@/assets/memes/zuba_spitting_render_stuff.jpg';
-import render_grapics_programming_vs_enterprise_developer from '@/assets/memes/render_grapics_programming_vs_enterprise_developer.jpg';
-import zuba_is_online from '@/assets/memes/zuba_is_online.jpg';
-import devtherapy_game_devtherapy from '@/assets/memes/devtherapy_game_devtherapy.jpg';
-import devtherapy_becoming_gamedev from '@/assets/memes/devtherapy_becoming_gamedev.jpg';
-import shemosavliani_kursi from '@/assets/memes/shemosavliani_kursi.png';
-import c_sharp_yvelgan_yoveltvis from '@/assets/memes/c_sharp_yvelgan_yoveltvis.png';
-import refactor_codebase from '@/assets/memes/refactor_codebase.jpg';
-import tornike_avto_dayra from '@/assets/memes/tornike_avto_dayra.jpg';
-import jansagi_opengl_dev from '@/assets/memes/jansagi_opengl_dev.png';
-import web_dev_swavlis_dro from '@/assets/memes/web_dev_swavlis_dro.png';
-import sami_weliwadia_vadebageb from '@/assets/memes/sami_weliwadia_vadebageb.jpg';
-import mexute_fix_me from '@/assets/memes/mexute_fix_me.jpg';
-import vimshi_dagaviwyda_caps from '@/assets/memes/vimshi_dagaviwyda_caps.mp4';
-import chemi_dzma_rockstar from '@/assets/memes/chemi_dzma_rockstar.jpg';
-import vibe_coders_roca from '@/assets/memes/vibe_coders_roca.jpg';
-import silence_tdd_edd_talking from '@/assets/memes/silence_tdd_edd_talking.png';
-import ts_developeri_moemarteba from '@/assets/memes/ts_developeri_moemarteba.png';
-import ukan_daixie_js_developero from '@/assets/memes/ukan_daixie_js_developero.jpg';
-
 export const memes = [
-  zuba_nina_avto,
-  avto_runaway,
-  avto_toreto,
-  avto_toreto_rav4,
-  intimuri_shuqi,
-  dotneteri_neli_kodi,
-  xari_xartan,
-  grdzeli_meme,
-  tornike_everyone,
-  main_chat,
-  game_jam_sachmeli,
-  zuba_car_story,
-  ai_es_c_sharp,
-  tornikes_multiverse,
-  abela_usulo_tamashi,
-  zuba_deleting_videos,
-  three_devs_one_bug_infinite_theories,
-  abelix_x_zuba,
-  three_babies,
-  tornike_shaurma,
-  excel_bug,
-  ese_ambavi_martali,
-  zuba_giyvarvar_tu_gkidivar,
-  c_vs_java_juniors,
-  zuba_is_typing,
-  tornike_looks_soso,
-  male,
-  biznes_idea,
-  dotnet_vs_gta,
-  mezoblis_bavshvma_gta_chaiwera,
-  gta_developer_vs_of_model,
-  uxilavi_folderiani_dzmakaci,
-  dotneteri,
-  avto_loves_hs,
-  tornike_gasaubrebas_atarebs,
-  zuba_roca_gaigo_rom,
-  zuba_x_dotnet_sastavi,
-  c_sharp_is_slow,
-  website_will_be_dotnet,
-  needtherapy,
-  avto_draw_25,
-  avto_smilling,
-  tornike_silenced,
-  avto_eating_shawarma,
-  dotnet_neli_tua,
-  magla_isari_avagde,
-  norqiqa_muzit_xelshi,
-  utimod_ro_avawyo,
-  avto_hate_c_sharp,
-  avto_lifts_c_sharp,
-  zubas_yoveldgiuroba,
-  konkretulad_rashia_dotnet_neli,
-  dotnet_hate_kompetencia,
-  avto_x_dotnet_meetup_members,
-  enterprise_devs_vs_other_devs,
-  avto_sibereshi,
-  avto_heartstone_wageba,
-  avto_hobbies,
-  tornike_asks_render_priest_for_blessing,
-  avto_c_sharp_demigod_card,
-  zuba_x_dotneteri_avto,
-  avto_c_sharp_pose,
-  c_sharp_shekreba,
-  va_enterprise_dev,
-  tornike_vs_avto,
-  avto_enterprise_vs_t_z_r,
-  zubas_gta_leak_uxsene,
-  dotnet_cudia,
-  avto_dancing,
-  zuba_x_saba_engine,
-  merabi_trump,
-  zuba_win_his_game_jam,
-  devtherapy_after_midnight,
-  tornike_x_teen_titans,
-  zuba_no_leak_gta,
-  tornike_swift_card,
-  avto_c_sharp_card,
-  devtherapy_random_discussions,
-  devtherapy_dgisit_gamit,
-  devtherapy_private_party,
-  devtherapy_paraskevs_cota_xanshi_v2,
-  devtherapy_paraskevs_cota_xanshi,
-  devtherapy_fear_cota_xanshi,
-  devtherapy_cota_xanshi,
-  tornike_dotnet_dajma,
-  t_a_fighting_X_r_sharing_links,
-  devtherapy_episode_start,
-  gta_sesxi,
-  render_samsaxuri_banki,
-  trump_x_devtherapy,
-  devtherapy_render_politics,
-  devtherapy_politics,
-  kurdebi_dependece_injection,
-  maikebis_shekveta,
-  how_mau_pg_look,
-  ar_aris_es_tvaleti,
-  tamashis_ideebi,
-  ra_waiget_tvini_am_renderit,
-  renderit_samsaxurs_vishovit_xo,
-  axali_tamashi_davwerot,
-  everyone_reading_render_stuff,
-  zuba_spitting_render_stuff,
-  render_grapics_programming_vs_enterprise_developer,
-  zuba_is_online,
-  devtherapy_game_devtherapy,
-  devtherapy_becoming_gamedev,
-  shemosavliani_kursi,
-  c_sharp_yvelgan_yoveltvis,
-  refactor_codebase,
-  tornike_avto_dayra,
-  jansagi_opengl_dev,
-  web_dev_swavlis_dro,
-  sami_weliwadia_vadebageb,
-  mexute_fix_me,
-  vimshi_dagaviwyda_caps,
-  chemi_dzma_rockstar,
-  vibe_coders_roca,
-  silence_tdd_edd_talking,
-  ts_developeri_moemarteba,
-  ukan_daixie_js_developero,
+  '/memes/zuba_nina_avto.png',
+  '/memes/avto_runaway.mp4',
+  '/memes/avto_toreto_rav4.png',
+  '/memes/avto_toreto.png',
+  '/memes/intimuri_shuqi.jpg',
+  '/memes/dotneteri_neli_kodi.png',
+  '/memes/xari_xartan.png',
+  '/memes/grdzeli_meme.png',
+  '/memes/tornike_everyone.mov',
+  '/memes/main_chat.jpg',
+  '/memes/game_jam_sachmeli.jpg',
+  '/memes/zuba_car_story.mp4',
+  '/memes/ai_es_c_sharp.jpg',
+  '/memes/tornikes_multiverse.png',
+  '/memes/abela_usulo_tamashi.jpg',
+  '/memes/zuba_deleting_videos.png',
+  '/memes/three_devs_one_bug_infinite_theories.png',
+  '/memes/abelix_x_zuba.png',
+  '/memes/three_babies.png',
+  '/memes/tornike_shaurma.mp4',
+  '/memes/excel_bug.png',
+  '/memes/ese_ambavi_martali.png',
+  '/memes/zuba_giyvarvar_tu_gkidivar.png',
+  '/memes/c_vs_java_juniors.jpg',
+  '/memes/zuba_is_typing.jpg',
+  '/memes/tornike_looks_soso.png',
+  '/memes/male.jpg',
+  '/memes/biznes_idea.mp4',
+  '/memes/dotnet_vs_gta.jpg',
+  '/memes/mezoblis_bavshvma_gta_chaiwera.jpg',
+  '/memes/gta_developer_vs_of_model.jpg',
+  '/memes/uxilavi_folderiani_dzmakaci.jpg',
+  '/memes/dotneteri.png',
+  '/memes/avto_loves_hs.png',
+  '/memes/tornike_gasaubrebas_atarebs.jpg',
+  '/memes/zuba_roca_gaigo_rom.jpg',
+  '/memes/zuba_x_dotnet_sastavi.jpg',
+  '/memes/c_sharp_is_slow.png',
+  '/memes/website_will_be_dotnet.jpg',
+  '/memes/needtherapy.mp4',
+  '/memes/avto_draw_25.png',
+  '/memes/avto_smilling.png',
+  '/memes/tornike_silenced.png',
+  '/memes/avto_eating_shawarma.mp4',
+  '/memes/dotnet_neli_tua.png',
+  '/memes/magla_isari_avagde.png',
+  '/memes/norqiqa_muzit_xelshi.png',
+  '/memes/utimod_ro_avawyo.jpg',
+  '/memes/avto_hate_c_sharp.png',
+  '/memes/avto_lifts_c_sharp.jpg',
+  '/memes/zubas_yoveldgiuroba.png',
+  '/memes/konkretulad_rashia_dotnet_neli.jpg',
+  '/memes/dotnet_hate_kompetencia.jpg',
+  '/memes/avto_x_dotnet_meetup_members.jpg',
+  '/memes/enterprise_devs_vs_other_devs.jpg',
+  '/memes/avto_sibereshi.jpg',
+  '/memes/avto_heartstone_wageba.jpg',
+  '/memes/avto_hobbies.jpg',
+  '/memes/tornike_asks_render_priest_for_blessing.mp4',
+  '/memes/avto_c_sharp_demigod_card.png',
+  '/memes/zuba_x_dotneteri_avto.jpg',
+  '/memes/avto_c_sharp_pose.jpg',
+  '/memes/c_sharp_shekreba.jpg',
+  '/memes/va_enterprise_dev.jpg',
+  '/memes/tornike_vs_avto.mp4',
+  '/memes/avto_enterprise_vs_t_z_r.mp4',
+  '/memes/zubas_gta_leak_uxsene.mp4',
+  '/memes/dotnet_cudia.png',
+  '/memes/avto_dancing.mp4',
+  '/memes/zuba_x_saba_engine.png',
+  '/memes/merabi_trump.jpg',
+  '/memes/zuba_win_his_game_jam.mp4',
+  '/memes/devtherapy_after_midnight.gif',
+  '/memes/tornike_x_teen_titans.jpg',
+  '/memes/zuba_no_leak_gta.jpg',
+  '/memes/tornike_swift_card.png',
+  '/memes/avto_c_sharp_card.png',
+  '/memes/devtherapy_random_discussions.jpg',
+  '/memes/devtherapy_dgisit_gamit.jpg',
+  '/memes/devtherapy_private_party.gif',
+  '/memes/devtherapy_paraskevs_cota_xanshi_v2.jpg',
+  '/memes/devtherapy_paraskevs_cota_xanshi.jpg',
+  '/memes/devtherapy_fear_cota_xanshi.jpg',
+  '/memes/devtherapy_cota_xanshi.jpg',
+  '/memes/tornike_dotnet_dajma.jpg',
+  '/memes/t_a_fighting_X_r_sharing_links.mp4',
+  '/memes/devtherapy_episode_start.mp4',
+  '/memes/gta_sesxi.jpg',
+  '/memes/render_samsaxuri_banki.jpg',
+  '/memes/trump_x_devtherapy.jpg',
+  '/memes/devtherapy_render_politics.jpg',
+  '/memes/devtherapy_politics.jpg',
+  '/memes/kurdebi_dependece_injection.jpg',
+  '/memes/maikebis_shekveta.png',
+  '/memes/how_mau_pg_look.png',
+  '/memes/ar_aris_es_tvaleti.jpg',
+  '/memes/tamashis_ideebi.jpg',
+  '/memes/ra_waiget_tvini_am_renderit.jpg',
+  '/memes/renderit_samsaxurs_vishovit_xo.jpg',
+  '/memes/axali_tamashi_davwerot.jpg',
+  '/memes/everyone_reading_render_stuff.jpg',
+  '/memes/zuba_spitting_render_stuff.jpg',
+  '/memes/render_grapics_programming_vs_enterprise_developer.jpg',
+  '/memes/zuba_is_online.jpg',
+  '/memes/devtherapy_game_devtherapy.jpg',
+  '/memes/devtherapy_becoming_gamedev.jpg',
+  '/memes/shemosavliani_kursi.png',
+  '/memes/c_sharp_yvelgan_yoveltvis.png',
+  '/memes/refactor_codebase.jpg',
+  '/memes/tornike_avto_dayra.jpg',
+  '/memes/jansagi_opengl_dev.png',
+  '/memes/web_dev_swavlis_dro.png',
+  '/memes/sami_weliwadia_vadebageb.jpg',
+  '/memes/mexute_fix_me.jpg',
+  '/memes/vimshi_dagaviwyda_caps.mp4',
+  '/memes/chemi_dzma_rockstar.jpg',
+  '/memes/vibe_coders_roca.jpg',
+  '/memes/silence_tdd_edd_talking.png',
+  '/memes/ts_developeri_moemarteba.png',
+  '/memes/ukan_daixie_js_developero.jpg',
 ];
